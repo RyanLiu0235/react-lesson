119ac6cd3863d1ca5ccdfda1f4518a6c5b715569
stop2stare <emitremmus0235@163.com>
2016-07-12 00:33:15 +0800

    将服务器的路由独立出来

--- dataServer/server.js
+++ dataServer/server.js
@@ -10,39 +10,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/todo', todo);
-var data = {
-    todo: {
-        1467726253744: {
-            content: '吃饭饭',
-            complete: 1468216457448
-        },
-        1467726253746: {
-            content: '睡觉觉',
-            complete: undefined
-        },
-        1467726253749: {
-            content: '打豆豆',
-            complete: 1468216458448
-        }
-    }
-}
-
-app.get('/api/todo', function(req, res, next) {
-	var id = req.query.id;
-
-	if (!id) return res.json(data.todo);
-	res.json(data.todo[id]);
-    
-});
-
-app.post('/api/todo', function(req, res, next) {
-    var newTodo = req.body;
-    data.todo[newTodo.id] = {
-        content: newTodo.content,
-        complete: newTodo.complete
-    }
-
-})
 
 app.listen(5100, function() {
 	console.log('server is running at http://localhost:5100');
