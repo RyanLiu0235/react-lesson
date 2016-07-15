c6aabe6c1f7e658f4ab20b19d45d4fa432acb32a
stop2stare <emitremmus0235@163.com>
2016-07-15 13:46:14 +0800

    数据服务器增加redux接口

--- dataServer/server.js
+++ dataServer/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var todo = require('./routes/todo');
+var redux = require('./routes/redux');
 
 var app = express();
 app.use(cors());
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/todo', todo);
+app.use('/api/redux', redux);
 
 app.listen(5100, function() {
 	console.log('server is running at http://localhost:5100');
